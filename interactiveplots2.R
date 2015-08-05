1578faa128a927ac279866318d83075fcfb3318a
jlow2499 <jlowhorn39@yahoo.com>
2015-08-05 08:27:16 -0400

    Create interactiveplots2.R

---
+++ interactiveplots2.R
@@ -0,0 +1,13 @@
+# Change the radiobuttons widget to a text widget 
+mtcars %>% 
+  ggvis(~mpg, ~wt, 
+        fill := input_text(label = "Choose color:", 
+                           value = "black")) %>% 
+  layer_points()
+
+# Map the fill property to a select box that returns variable names
+mtcars %>% 
+  ggvis(~mpg, ~wt, 
+        fill = input_select(label = "Choose fill variable:", 
+                            choices = names(mtcars), map = as.name)) %>% 
+  layer_points()
